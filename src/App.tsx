5f028ab60f13d2f9f9c65d6a1a6fc34dbf531602
Denis Bezukladnikov <denis3094@mail.ru>
2023-06-26 22:26:59 +0700

    feat: создать компоненту task list

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import './App.css'
 
 function App() {
 
-  return (
-    <>
-    </>
-  )
+    return (
+        <div className="App">
+            <TaskList/>
+        </div>
+    )
 }
 
 export default App
